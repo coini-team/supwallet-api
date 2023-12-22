353bb7943b4fc5abdc97094f763a29268f78676a
Jonathan Nolasco <jnolascob@gmail.com>
2023-12-22 13:23:17 -0500

    Create wallets tables

---
+++ src/modules/wallet/entities/wallet.entity.ts
@@ -0,0 +1,24 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { walletStatus } from 'src/shared/enums/wallet-status.enum';
+
+@Entity()
+export class Wallet {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 50 })
+  address: string;
+
+  @Column({ name: 'private_key', length: 70 })
+  privateKey: string;
+
+  @Column({ length: 200 })
+  seedphrase: string;
+
+  @Column({
+    type: 'enum',
+    enum: walletStatus,
+    default: walletStatus.ACTIVE,
+  })
+  status: string;
+}
---
+++ src/shared/enums/wallet-status.enum.ts
@@ -0,0 +1,5 @@
+export enum walletStatus {
+    ACTIVE = 'active',
+    INACTIVE = 'inactive',
+  }
+  
